359a4c861efe0d9ddefee8abcaafe8129c1bdf12
Solitario007 <danielpgiovanny@hotmail.com>
2021-01-20 16:32:59 -0500

    Add files via upload

---
+++ myProjectMaven/src/test/java/com/org/PruebaMaven/myProjectMaven/PruebaTest.java
@@ -0,0 +1,14 @@
+package com.org.PruebaMaven.myProjectMaven;
+
+import org.junit.Test;
+
+public class PruebaTest {
+	
+	@Test
+	public void imprimeAlgo() {
+		
+		System.out.println("Esto es una prueba de Maven con Eclipse");
+	}
+
+	
+}
